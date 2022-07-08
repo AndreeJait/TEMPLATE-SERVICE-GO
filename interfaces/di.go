81d192c484c736239749186052edef04c3124e51
Andree Panjaitan <panjaitanandree@gmail.com>
2022-07-08 17:45:06 +0700

    Init Template

--- interfaces/di.go
+++ interfaces/di.go
@@ -1,19 +1,10 @@
 package interfaces
 
 import (
-	"github.com/Del-Gong/DELGONG-BE-STORY/interfaces/auth"
-	"github.com/Del-Gong/DELGONG-BE-STORY/interfaces/story"
-	"github.com/pkg/errors"
 	"go.uber.org/dig"
 )
 
 func Register(container *dig.Container) error {
-	if err := container.Provide(auth.NewViewService); err != nil {
-		return errors.Wrap(err, "failed to provide Authentication")
-	}
-
-	if err := container.Provide(story.NewViewService); err != nil {
-		return errors.Wrap(err, "failed to provide Story")
-	}
+	// Register Controller here
 	return nil
 }
